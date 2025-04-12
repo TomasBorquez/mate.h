15919b141fb3be178acfcab7614fb0df5f1733db
TomasBorquez <tomasborquez13@gmail.com>
2025-04-11 22:39:01 -0300

    Remove defer

--- mate.h
+++ mate.h
@@ -332,30 +332,6 @@ void LogError(const char *format, ...) FORMAT_CHECK(1, 2);
 void LogSuccess(const char *format, ...) FORMAT_CHECK(1, 2);
 void LogInit();
 
-#ifdef COMPILER_GCC
-#define defer __DEFER(__COUNTER__)
-#define __DEFER(N) __DEFER_(N)
-#define __DEFER_(N) __DEFER__(__DEFER_FUNCTION_##N, __DEFER_VARIABLE_##N)
-#define __DEFER__(F, V)                                                                                                                                                                                                                        \
-  auto void F(int *);                                                                                                                                                                                                                          \
-  [[gnu::cleanup(F)]] int V;                                                                                                                                                                                                                   \
-  auto void F(int *)
-
-#elif defined(COMPILER_CLANG)
-// NOTE: Must compile with -fblocks
-typedef void (^const __df_t)(void);
-
-[[maybe_unused]]
-static inline void __df_cb(__df_t *__fp) {
-  (*__fp)();
-}
-
-#define defer __DEFER(__COUNTER__)
-#define __DEFER(N) __DEFER_(N)
-#define __DEFER_(N) __DEFER__(__DEFER_VARIABLE_##N)
-#define __DEFER__(V) [[gnu::cleanup(__df_cb)]] __df_t V = ^void(void)
-#endif
-
 /* Mate */
 typedef struct {
   i64 lastBuild;
