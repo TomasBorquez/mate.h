16c21dbfb26ae9fd401b2f0c6aca684912496860
TomasBorquez <tomasborquez13@gmail.com>
2025-05-07 16:11:35 -0300

    Fix examples, better README

--- examples/04-raylib-build/mate.c
+++ examples/04-raylib-build/mate.c
@@ -27,7 +27,7 @@ i32 main() {
     String exePath = InstallExecutable();
     errno_t errExe = RunCommand(exePath);
     errno_t errCompileCmd = CreateCompileCommands();
-    result = errExe == 0 && errCompileCmd == 0;
+    result = (errExe == 0 && errCompileCmd == 0) ? 0 : 1; // 0 success, >1 error
   }
   EndBuild();
 
